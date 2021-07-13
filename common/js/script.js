e967b3a866cbe48dd1694f6c96b40829bcecb4f1
Nishino Satoshi <nishino.satoshi.2532@members.co.jp>
2021-07-14 00:31:25 +0900

    develop

--- common/js/script.js
+++ common/js/script.js
@@ -1,5 +1,6 @@
 const statuses = ['hp', 'mp', 'str', 'def', 'int', 'res', 'agi'];
-const character_num = 2;
+const actor_num = 4;
+const actors = [];
 
 const sha256 = async (text) => {
   const uint8 = new TextEncoder().encode(text);
@@ -7,30 +8,64 @@ const sha256 = async (text) => {
   return Array.from(new Uint8Array(digest)).map(v => v.toString(16).padStart(2, '0')).join('');
 }
 
-const hash = async (character_name) => await sha256(character_name);
+const hash = async (actor_name) => await sha256(actor_name);
 
-class Character {
-  static async init(character_name) {
-    const character = new Character();
-    const name_hash = await hash(character_name);
-    statuses.forEach((status, index) => character[status] = parseInt(name_hash.slice(index * 8, index * 8 + 7), 16) % 100);
-    return character;
+const addHistory = text => document.getElementById('battleHistory').textContent += `${text}\n`;
+
+class Actor {
+  static async init(actor_name) {
+    const actor = new Actor();
+    actor.name = actor_name;
+    const name_hash = await hash(actor_name);
+
+    statuses.forEach((status, index) => actor[status] = parseInt(name_hash.slice(index * 8, index * 8 + 7), 16) % 100);
+    return actor;
+  }
+
+  attack() {
+    const rand_hit = Math.ceil(Math.random() * 100);
+    const rand_pow = (Math.round(Math.random() * 20) + 90) / 100;
+    addHistory(`${this.name} の こうげき！`);
+
+    if (rand_hit <= 10) {
+      addHistory('外れてしまった！');
+      return false;
+    } else if (rand_hit > 90) {
+      addHistory('かいしん の いちげき！');
+      return Math.round(this.str * rand_pow * 1.5);
+    } else {
+      return Math.round(this.str * rand_pow);
+    }
+  }
+
+  defend(pow, index) {
+    let damage = pow - this.def;
+    if (damage <= 0) damage = 1;
+    addHistory(`${this.name} は ${damage} の ダメージをうけた！`);
+    this.hp -= damage;
+
+    if (this.hp < 0) {
+      this.hp = 0;
+      addHistory(`\n${this.name} は ちからつきた！`);
+    }
+
+    document.getElementById(`actor${index}_hp`).textContent = this.hp;
   }
 }
 
-const characterHTML = (num) => {
+const actorHTML = (num) => {
   let status_list = '';
   statuses.forEach(status => {
     status_list += `
       <li class="status_item">
         <p class="status_title">${status.toUpperCase()}</p>
-        <span id="character${num}_${status}"></span>
+        <span id="actor${num}_${status}"></span>
       </li>
     `
   });
   return `
-    <div class="character_container">
-      <input type="text" id="character${num}" value="${num}">
+    <div class="actor_container">
+      <input type="text" id="actor${num}" value="${num}">
       <ul class="status_list">
         ${status_list}
       </ul>
@@ -38,18 +73,58 @@ const characterHTML = (num) => {
   `
 }
 
-const set_status = async (num, character) => {
-  statuses.forEach(status => document.getElementById(`character${num}_${status}`).textContent = character[status]);
+const set_status = async (actor, index) => {
+  statuses.forEach(status => document.getElementById(`actor${index}_${status}`).textContent = actor[status]);
 }
 
 window.onload = function () {
-  for (let i = 1; i <= character_num; i++) document.getElementById('characterWrapper').innerHTML += characterHTML(i);
-  document.getElementById('startButton').addEventListener('click', async () => {
-    for (let i = 1; i <= character_num; i++) {
-      const character_name = document.getElementById(`character${i}`).value;
-      if (!character_name) break;
-      const character = await Character.init(character_name);
-      await set_status(i, character);
+  const $setButton = document.getElementById('setButton');
+  const $startButton = document.getElementById('startButton');
+  const $resetButton = document.getElementById('resetButton');
+  let sorted_actors = [];
+
+  for (let i = 0; i < actor_num; i++) document.getElementById('actorWrapper').innerHTML += actorHTML(i);
+
+  $setButton.addEventListener('click', async () => {
+    for (let i = 0; i < actor_num; i++) {
+      const actor_name = document.getElementById(`actor${i}`).value;
+      actors.push(await Actor.init(actor_name)); //アクターを生成して配列にプッシュ
     }
+
+    actors.forEach(async (actor, index) => {
+      await set_status(actor, index);
+    });
+
+    sorted_actors = actors.slice();
+    sorted_actors.sort((a, b) => b.agi - a.agi); //素早さ順に並び替え
+
+    $setButton.style.display = 'none';
+    $startButton.style.display = 'block';
+    $resetButton.style.display = 'block';
+  });
+
+  $startButton.addEventListener('click', () => {
+    while (sorted_actors.length > 1) {
+      for (let i = 0; i < sorted_actors.length; i++) {
+        const pow = sorted_actors[i].attack();
+        if (pow) {
+          const arr = sorted_actors.filter(n => n !== sorted_actors[i]);
+          const targeted = arr[Math.floor(Math.random() * arr.length)];
+          let targeted_index;
+
+          actors.forEach((actor, index) => { if (actor.name === targeted.name) targeted_index = index });
+          targeted.defend(pow, targeted_index);
+
+          if (targeted.hp === 0) {
+            sorted_actors = sorted_actors.filter(n => n !== targeted);
+            if (sorted_actors.length === 1) break;
+          }
+        }
+        addHistory('');
+
+      }
+    }
+
+    addHistory(`\n${sorted_actors[0].name} の しょうり！`);
   });
 }
